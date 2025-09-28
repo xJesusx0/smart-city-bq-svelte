f2f0e1bd798dbede303ab198e127ddecb8249401
Daniel Dussan <daniel2014dussan@gmail.com>
2025-09-27 19:17:01 -0500

    feat(api): setup base API constants and path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,15 @@ import tailwindcss from "@tailwindcss/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
+import path from "path";
+
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	resolve: {
+		alias: {
+			$lib: path.resolve(__dirname, "src/lib"),
+			"@components": path.resolve(__dirname, "src/lib/components"),
+			"@utils": path.resolve(__dirname, "src/lib/utils")
+		}
+	}
 });
